caa7c8989c1c9cf85e4c13274f3f4ee5e7be2b13
Balaji <balaji.om@optisolbusiness.com>
2021-02-07 12:34:37 +0530

    Remove unwanted packages

--- App.js
+++ App.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 import { SafeAreaView } from 'react-native';
 import Sandbox from "./components/flexUsage.js"
 
-// import { RNCamera } from 'react-native-camera';
-
-
 class App extends Component{
     render(){
         return(
@@ -17,20 +14,3 @@ class App extends Component{
 }
 
 export default App
-
-// const styles = StyleSheet.create({
-//     container:{
-//         flex:1,
-//         alignContent:'center',
-//     },
-//     inputtext:{
-//         height: 40,
-//         marginTop:15,
-//         borderColor:"darkcyan",
-//         borderWidth:1
-//     },
-//     buttonstyle:{
-//         paddingTop:15,
-//         marginTop:20
-//     }
-// })
